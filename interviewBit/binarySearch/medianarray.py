327852ebf42b4148ec544b628d0d99285a472bee
Mariko Wakabayashi <marimarimekko@gmail.com>
2015-10-26 14:27:58 -0500

    Solved medianarray problem

---
+++ interviewBit/binarySearch/medianarray.py
@@ -0,0 +1,73 @@
+'''There are two sorted arrays A and B of size m and n respectively.
+
+Find the median of the two sorted arrays ( The median of the array formed by merging both the arrays ).
+
+The overall run time complexity should be O(log (m+n)).
+
+Sample Input
+
+A : [1 4 5]
+B : [2 3]
+
+Sample Output
+
+3'''
+
+'''
+@author: Mariko Wakabayashi
+'''
+from nose.tools import assert_equal
+
+def findMedianSortedArrays(A, B):
+	m = len(A)
+	n = len(B)
+	if m > n:
+		m, n = n, m
+		A, B = B, A
+	if m == 0:
+		if n == 0:
+			return 0
+		elif n%2: #if n is odd
+			return B[n/2]
+		else:
+			return (B[n/2]+B[n/2-1])/2.0
+	low = 0
+	high = m
+	while low <= high:
+		i = (low+high) / 2
+		j = (m+n+1)/2-i
+
+		if (j == 0 or i == m or B[j - 1] <= A[i]) and (i == 0 or j == n or A[i-1] <= B[j]):
+			if (m+n)%2:
+				if i == 0:
+					return B[j-1]
+				elif j == 0:
+					return A[i-1]
+				else:
+					return max(A[i-1], B[j-1])
+			else:
+				if i == 0:
+					return (B[j-1] + min(A[i],B[j]))/2.0
+				if j == 0:
+					return (A[i-1] + min(A[i],B[j]))/2.0
+				if i == m:
+					return (max(A[i-1],B[j-1]) + B[j])/2.0
+				if j == n:
+					return (max(A[i-1],B[j-1]) + A[i])/2.0
+				return (max(A[i-1],B[j-1]) + min(A[i],B[j]))/2.0
+		elif (j == 0 or i == m or B[j - 1] > A[i]):
+			low = i+1
+		elif (i == 0 or j == n or A[i-1] > B[j]):
+			high = i-1
+	return -1
+
+
+
+if __name__ == '__main__':
+		array1 = [6, 7, 8]
+		array2 = [1, 2, 2, 3, 3, 3]
+		if assert_equal(findMedianSortedArrays(array1, array2),3) == None:
+			print("Success!")
+
+
+
